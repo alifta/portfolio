6c34c2e0a168994a1ef7b716027fc9042bc35634
Ali <af4166@mun.ca>
2022-02-22 20:00:04 -0330

    Adding database.txt

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for, request, redirect
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
@@ -13,11 +13,20 @@ def html_page(page_name):
     return render_template(page_name)
 
 
+def write_to_file(data):
+    with open('database.txt', mode='a') as database:
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        file = database.write(f'\n{email},{subject},{message}')
+
+
 @app.route("/submit_form", methods=["POST", "GET"])
 def submit_form():
     if request.method == "POST":
         data = request.form.to_dict()
-        print(data)
+        write_to_file(data)
+        # print(data)
         return redirect("/thankyou.html")
     else:
         return "Something went wrong, try again."
